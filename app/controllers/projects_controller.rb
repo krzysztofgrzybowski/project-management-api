c2bcc14f50844279c63d636bc4fa7efd1ee9143d
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-06-26 17:52:59 +0200

    Add API for creating projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,21 @@ class ProjectsController < ApplicationController
   def index
     render json: current_user.owned_projects.order(:name), status: :ok
   end
+
+  def create
+    project = current_user.owned_projects.new(project_params)
+    if project.save
+      render json: project, status: :ok
+    else
+      render json: { errors: project.errors }, status: :unprocessable_rntity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :name
+    )
+  end
 end
