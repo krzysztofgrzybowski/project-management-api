6fd8e0040ba6e4516547c558490062053f616b2f
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-06-28 17:24:01 +0200

    Add controller for project assignments

---
+++ app/controllers/project_assignments_controller.rb
@@ -0,0 +1,33 @@
+class ProjectAssignmentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project, only: [:update, :destroy]
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_project_assignment
+    @project_assignment = ProjectAssignment.find_by(
+                            user_id: params[:user_id],
+                            project_id: params[:project_id]
+                          )
+  end
+
+  def project_assignment_params
+    params.require(:project_assignment).permit(
+      :user_id,
+      :project_id,
+      :role
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   resources :projects
+
+  resources :project_assignments, only: [:create, :update, :destroy]
 end
