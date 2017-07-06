3744f90a6fd8f97f8c296a9d76d5587cea8dc1f1
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-07-06 17:14:02 +0200

    Add API for project assignment create and update

--- app/controllers/project_assignments_controller.rb
+++ app/controllers/project_assignments_controller.rb
@@ -3,15 +3,27 @@ class ProjectAssignmentsController < ApplicationController
   before_action :set_project_assignment, only: [:update, :destroy]
 
   def create
-
+    user = User.find_by(email: params[:email])
+    unless user
+      user = User.invite!(email: params[:email])
+    end
+    project_assignment = ProjectAssignment.new(user_id: user.id, project_id: params[:project_id], role: params[:role])
+    if project_assignment.save
+      render json: project_assignment, status: :ok
+    else
+      render json: { errors: project_assignment.errors }, status: :unprocessable_entity
+    end
   end
 
   def update
-
+    if @project_assignment.update(project_assignment_params)
+      render json: @project_assignment, status: :ok
+    else
+      render json: { errors: @project_assignment.errors }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
   end
 
   private
